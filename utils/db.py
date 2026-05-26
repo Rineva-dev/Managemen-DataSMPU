61453969ff1af91773a8326d8a3a1fa841ff1eb6
rizal <rhyzalkurniawan97@gmail.com>
2026-05-26 18:43:34 +0800

    upgrade css modal

--- utils/db.py
+++ utils/db.py
@@ -1,10 +1,23 @@
 import sqlite3
+import os
 
-DB_PATH = "/data/database.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# =========================
+# DATABASE PATH
+# =========================
+if os.name == "nt":  # Windows (LOCAL)
+    DB_PATH = os.path.join(BASE_DIR, "..", "database.db")
+else:  # Linux / Docker / Dokploy
+    DB_PATH = os.getenv("DB_PATH", "/data/database.db")
 
 print("DB PATH AKTIF:", DB_PATH)
 
+
 def db():
+    # pastikan folder DB ada
+    os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+
     conn = sqlite3.connect(
         DB_PATH,
         timeout=30,
