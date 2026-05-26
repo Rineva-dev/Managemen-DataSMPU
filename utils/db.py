d870e28fca04e234a76af6dc81f62424bd96ec75
rizal <rhyzalkurniawan97@gmail.com>
2026-05-26 14:54:44 +0800

    fix postgres connection

--- utils/db.py
+++ utils/db.py
@@ -1,13 +1,12 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import os
 
 def db():
     return psycopg2.connect(
-        host=os.getenv("managemen-data-smpu-db-bdhy2r"),
-        port=os.getenv("5432"),
-        database=os.getenv("smpu_db"),
-        user=os.getenv("database_user"),
-        password=os.getenv("ManagementSMPU123"),
+        host="managemen-data-smpu-db-bdhy2r",
+        port=5432,
+        database="smpu_db",
+        user="database_user",
+        password="ManagementSMPU123",
         cursor_factory=RealDictCursor
     )
