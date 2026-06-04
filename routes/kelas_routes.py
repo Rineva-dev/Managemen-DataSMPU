942fc25c07e076bcc90bdbe26c545f664bc3b1e2
rizal <rhyzalkurniawan97@gmail.com>
2026-06-04 11:28:46 +0800

    upgrade edit nilai temlate

--- routes/kelas_routes.py
+++ routes/kelas_routes.py
@@ -447,7 +447,7 @@ def get_mapel_kelas(kelas_id):
         mapel = d.execute("""
             SELECT id, nama, jenis
             FROM mata_pelajaran
-            WHERE jenis IN ('wajib', 'mulok', 'kegiatan')
+            WHERE jenis IN ('wajib', 'mulok')
             ORDER BY nama ASC
         """).fetchall()
         
