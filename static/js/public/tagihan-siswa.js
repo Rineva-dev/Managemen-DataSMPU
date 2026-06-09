8c726767b4cbcf3cbdb8d5cb45b0d89b95599928
rizal <rhyzalkurniawan97@gmail.com>
2026-06-09 19:19:02 +0800

    upgrade pembayaran siswa

--- static/js/public/tagihan-siswa.js
+++ static/js/public/tagihan-siswa.js
@@ -94,7 +94,8 @@ async function addToCart(data, cardElement) {
             method: "POST",
 
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "X-CSRFToken": csrfToken
             },
 
             body: JSON.stringify({
