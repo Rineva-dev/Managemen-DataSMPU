5d31e7682d394794c70cf4fbaace2c93e58a8681
rizal <rhyzalkurniawan97@gmail.com>
2026-06-19 09:24:28 +0800

    upgrade css profile dropdown payment student

---
+++ static/js/keuangan/jenis-pembayaran.js
@@ -0,0 +1,11 @@
+function openTambahPembayaran() {
+    document
+        .getElementById("modalPembayaran")
+        .classList.add("show");
+}
+
+function closeModal() {
+    document
+        .getElementById("modalPembayaran")
+        .classList.remove("show");
+}
